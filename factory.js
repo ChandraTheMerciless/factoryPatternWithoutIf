5c152c356033cdfa5e9c398e057c40947403baaa
Chandra <chandra.asar@gmail.com>
2016-06-15 19:51:23 -0400

    factory pattern - will look through this again for good practice before calling it complete

---
+++ factory.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function AnimalCall(){
+    var animals = {
+        'cat': 'meow',
+        'dog': 'arf',
+        'snake': 'hiss',
+        'human': 'blah blah blah',
+        'mouse': 'squeak'
+    };
+
+    function createAnimalCall(animal){
+        return animals[animal] || "mysterious animal call";
+    };
+
+    return {
+        'createAnimalCall': createAnimalCall
+    }
+};
+
+function getAnimalCall(){
+    var animal = new AnimalCall();
+    var call = animal.createAnimalCall(anim.value);
+    console.log(call);
+};
+
+var anim = document.getElementById('animal');
+
+anim.addEventListener('blur', getAnimalCall);
